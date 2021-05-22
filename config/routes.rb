4b444fbb75670c971375247c5d7b9315079450a2
priyadikshit911 <67967502+priyadikshit911@users.noreply.github.com>
2021-05-22 18:59:06 +0530

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  # get 'home/index'
+  resources :friends
+  get 'home/about'
+  get 'home/test'
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
